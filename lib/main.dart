69d26369a8bdcdbfa753373350a8ed8c713799b3
eXOffice <sumontobonti@gmail.com>
2023-03-22 13:49:26 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chupi_chupi/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChupiChupi());
+}
+
+class ChupiChupi extends StatelessWidget {
+  const ChupiChupi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      theme: ,
+      home: WelComeScreen(),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelComeScreen extends StatefulWidget {
+  const WelComeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelComeScreen> createState() => _WelComeScreenState();
+}
+
+class _WelComeScreenState extends State<WelComeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
